2f287c63cd1277199d468f906a0cf9c04f54f270
arupsarkar <sarkar.arup@gmail.com>
2023-10-29 19:41:13 -0400

    Removed middleware

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -3,7 +3,7 @@ import NextAuth from "next-auth/next";
 import { config } from "../../../../../auth";
 
 const handler = NextAuth(config)
-//need to handle callbacks here
+
 
 export { handler as GET, handler as POST}
 
